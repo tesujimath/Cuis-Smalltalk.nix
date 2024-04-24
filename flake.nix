eaf911380dd586f590af8b5343539b674d5bfc05
Simon Guest <simon.guest@tesujimath.org>
2024-04-25 11:13:06 +1200

    Use buildFHSUserEnv to build env for Cuis

--- flake.nix
+++ flake.nix
@@ -7,27 +7,28 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
+        let pkgs = nixpkgs.legacyPackages.${system};
+            cuis_env = pkgs.buildFHSUserEnv {
+              name = "cuis-env";
+              targetPkgs = pkgs: (with pkgs;
+                [
+                  libGL
+                  libpulseaudio
+                  libuuid.lib
+                ]) ++ (with pkgs.xorg;
+                  [
+                    libICE
+                    libSM
+                    libX11
+                    libXext
+                    libXrender
+                  ]);
+              runScript = "bash";
+            };
+        in
         with pkgs;
         {
-          devShells.default = mkShell {
-            nativeBuildInputs = [
-              bashInteractive
-            ];
-            shellHook = let lib-pkgs = [
-              libGL
-              libpulseaudio
-              libuuid.lib
-              xorg.libICE
-              xorg.libSM
-              xorg.libX11
-              xorg.libXext
-              xorg.libXrender
-            ]; in ''
-              export NIX_LD="${glibc}/lib64/ld-linux-x86-64.so.2"
-              export NIX_LD_LIBRARY_PATH="${lib.strings.concatMapStringsSep ":" (p: p + "/lib") lib-pkgs}"
-            '';
-          };
+          packages.default = cuis_env;
         }
       );
 }
